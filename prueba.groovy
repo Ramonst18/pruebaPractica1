661e5018736a8e3b2ba62ceb90563952c878951a
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2022-04-06 17:04:01 -0700

    Se realizó la modificación necesaria

---
+++ prueba.groovy
@@ -0,0 +1,33 @@
+job('practicaPDS1'){
+    description('Tarea que nos dejo el profe')
+    scm {
+        git('https://github.com/Ramonst18/pruebaPractica1.git, 'main') { node ->
+      	    node / gitConfigName('Ramonst18')
+      	    node / gitConfigEmail('Ramonst18@hotmail.com')
+	      }
+    }
+    steps {
+        shell("prueba.SH")
+    }
+    publishers{
+        mailer("ramonlopezpracticas2@gmail.com", true, true)
+        mailer("ericaguzmanpdsi@gmail.com", true, true)
+        slackNotifier{
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(true)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(true)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+	      }
+    }
+}
